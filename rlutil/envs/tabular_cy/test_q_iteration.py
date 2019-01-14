59c4c5f3076b03a23a5fb3f20584ad5151cb357a
Justin Fu <justinjfu@gmail.com>
2019-01-14 00:58:22 -0800

    visitation tests

--- rlutil/envs/tabular_cy/test_q_iteration.py
+++ rlutil/envs/tabular_cy/test_q_iteration.py
@@ -3,7 +3,7 @@ import parameterized
 import numpy as np
 
 from rlutil.envs.tabular_cy import q_iteration, tabular_env
-from rlutil.envs.tabular_cy import q_iteration_py
+from rlutil.envs.tabular import q_iteration as q_iteration_py
 
 
 class QIterationTest(unittest.TestCase):
@@ -16,10 +16,33 @@ class QIterationTest(unittest.TestCase):
         'ent_wt': 1.0,
         'discount': 0.99,
     }
-    qvals_py = q_iteration_py.q_iteration_sparse(self.env, **params)
-    qvals_cy = q_iteration.q_iteration_sparse(self.env, **params)
+    qvals_py = q_iteration_py.softq_iteration(self.env, **params)
+    qvals_cy = q_iteration.softq_iteration(self.env, **params)
     self.assertTrue(np.allclose(qvals_cy, qvals_py))
 
+  def test_visitations(self):
+    env = tabular_env.CliffwalkEnv(num_states=3, transition_noise=0.00)
+    params = {
+        'num_itrs': 50,
+        'ent_wt': 0.0,
+        'discount': 0.99,
+    }
+    qvals_py = q_iteration_py.softq_iteration(env, **params)
+
+    visitations = q_iteration_py.compute_visitation(env, qvals_py, ent_wt=0.0, env_time_limit=1)
+    s_visitations = np.sum(visitations, axis=1)
+    tru_visits = np.array([1, 0, 0]) 
+    self.assertTrue(np.allclose(tru_visits, s_visitations))
+
+    visitations = q_iteration_py.compute_visitation(env, qvals_py, ent_wt=0.0, env_time_limit=3)
+    s_visitations = np.sum(visitations, axis=1)
+    tru_visits = np.array([1, 1, 1]) / 3.0
+    self.assertTrue(np.allclose(tru_visits, s_visitations))
+
+    visitations = q_iteration_py.compute_visitation(env, qvals_py, ent_wt=0.0, env_time_limit=5)
+    s_visitations = np.sum(visitations, axis=1)
+    tru_visits = np.array([2, 2, 1]) / 5.0
+    self.assertTrue(np.allclose(tru_visits, s_visitations))
 
 if __name__ == '__main__':
   unittest.main()
